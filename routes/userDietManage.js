ea74b069b521c962f1e6772778d5dcc7641fed00
hanseongPark <hsung901231@gmail.com>
2019-09-10 21:39:53 +0900

    Remark allRoutes

--- routes/userDietManage.js
+++ routes/userDietManage.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const {parseDietData} = require('./middlewares')
 const {Diet} = require('../models');
 
+//userDietManage.js는 user/dietmanage/로 들어오는 모든 ajax요청을 처리하기 위한 라우터입니다.
+//GET:/user/dietmanage 요청이 들어올 시 우선 passport 의 isAuthenticated() 메소드가 req 의
+//로그인 여부를 판단합니다. 이후 로그인 상태일 시에는 req.user 객체의 아이디와 클라이언트에서 보내온
+//prams date를 바탕으로 식단 정보를 찾습니다. 만약 식단 정보가 있다면 data를 보내고 없다면
+//null값을 보내게 됩니다.
 router.get('/', async(req,res)=>{
     if(req.isAuthenticated()){
         const data = await Diet.findAll({where:{user_id: req.user.id, date: req.query.date}})
@@ -17,15 +22,21 @@ router.get('/', async(req,res)=>{
     }
 })
 
+//POST:/user/dietmanage 요청은 사용자가 식단을 추가할 때의 요청을 처리하는 라우터입니다. 
+//우선 parseDietData 미들웨어를 통과하며 식단의 데이터들을 DB에 저장하기 적합한 자료형태로
+//변환합니다. 이후 req.body의 정보들을 그대로 저장한 후 저장이 완료되었다면 정보를 다시
+//json의 형태로 클라이언트에 보냅니다.
 router.post('/', parseDietData, async(req,res)=>{
     const save = await Diet.create(req.body)
     if(save){
-        const saveData = await Diet.findOne({where: req.body})
-        res.json(saveData);
+        res.json(save);
     }
-    
 })
 
+//POST:/user/dietmanage/time 은 유저가 클라이언트의 식단관리 중 하나를 클릭했을 때의 요청을
+//처리하는 라우터입니다. 우선 사용자의 아이디와 날짜 , 그리고 조회하기 원하는 식단리스트의 시간대를
+//바탕으로 DB를 검색합니다. 이후에 data가 있다면 그 값을 json 형태로 보내고, data가 없다면 null값을
+//반환합니다.
 router.post('/time', async(req,res)=>{
     const data = await Diet.findAll({
         where:{
@@ -42,6 +53,9 @@ router.post('/time', async(req,res)=>{
     
 })
 
+//DELETE:/user/dietmanage 요청은 유저가 식단관리 리스트를 삭제할 때 처리합니다. 먼저 유저의 id와 날짜를
+//요소로 데이터를 지웁니다. 이후 data를 성공적으로 지웠다면 다시 그날의 식단리스트를 다 찾은 후에
+//클라이언트로 보내어 식단 리스트를 갱신하도록 만듭니다.
 router.delete('/', async(req,res)=>{
     const data = await Diet.destroy({where:{id: req.body.id, date: req.body.date}})
     if(data){
--- routes/userWorkoutManage.js
+++ routes/userWorkoutManage.js
@@ -6,6 +6,10 @@ const router = express.Router();
 const {parseWorkoutData, deleteWorkout, parseDate, parseTarget} = require('./middlewares');
 const {Workout} = require('../models');
 
+//userWorkoutManage.js 는 유저의 운동정보에 관한 요청을 관리하고 처리하는 라우터들이 모여있습니다.
+///user/workout으로 들어오는 모든 ajax요청을 처리하게 됩니다.
+//GET:/user/workout 라우터는 유저가 운동 관리 페이지로 이동했을 때 요청됩니다.
+//해당 요일의 유저의 모든 운동 정보를 클라이언트에 json형태로 보내게 됩니다.
 router.get('/', async(req,res)=>{
     if(req.isAuthenticated()){
         const data = await Workout.findAll({where:{user_id: req.user.id, date: req.query.date}})
@@ -19,6 +23,11 @@ router.get('/', async(req,res)=>{
     }
 });
 
+//POST:/user/workout 은 유저가 운동관리 폼을 이용해 정보를 추가할 때 발생되는 요청입니다.
+//우선 요청된 정보는 3개의 미들웨어를 거칩니다. parseDate 를 통해 날짜를 변환합니다. 변환의 이유는 middlewares에서
+//더 자세히 확인할 수 있습니다. 이후 볼륨 정보 저장을 위해 parseTarget 미들웨어를 거치고 parseWorkoutData 에서 
+//각 운동을 부위별로 분류하여 각 볼륨데이터베이스에 저장합니다. 이후 남은 정보를 유저 workout 데이터에 저장하고
+//저장이 완료되면 json으로 데이터를 보내게 됩니다.
 router.post('/', parseDate, parseTarget, parseWorkoutData, async(req,res)=>{
     const save = await Workout.create({
         workname: req.body.workname, 
@@ -30,26 +39,24 @@ router.post('/', parseDate, parseTarget, parseWorkoutData, async(req,res)=>{
         user_id: req.user.id
     })
     if(save){
-        const saveData = await Workout.findOne({where: {            
-            workname: req.body.workname, 
-            target: req.body.target, 
-            reps: req.body.reps,
-            sets: req.body.sets,
-            workweight: req.body.workweight,
-            date: req.body.date,
-            user_id: req.user.id            
-        }})
-        res.json(saveData)
+        res.json(save)
     }else{
         res.json(null)
     }
 });
 
+//DELETE:/user/workout 요청은 사용자가 운동정보 리스트를 삭제하여 데이터의 변화가 일어날 때 발생합니다.
+//우선 parseDate, parseTarget 날짜를 변환하고 삭제할 운동리스트의 볼륨을 정합니다. 이후 deleteWorkout에서
+//대상 운동리스트를 삭제하고 볼륨리스트 역시 삭제된 리스트에 맞게 변경합니다. 마지막으로는 그 날 변경이
+//모두 완료된 모든 데이터를 사용자에게 json 형태로 보내게 됩니다.
 router.delete('/', parseDate, parseTarget, deleteWorkout, async(req,res)=>{
     const updateWorkout = await Workout.findAll({where:{user_id: req.user.id, date: req.body.date}})
     res.json(updateWorkout)
 });
 
+//POST:/user/workout/target 요청은 유저가 당일의 부위별 운동리스트를 확인할 때 발생합니다.
+//req.body에 담겨있는 유저id, 운동부위, 날짜를 바탕으로 모든 정보를 찾아내 클라이언트에
+//json으로 보내게 됩니다.
 router.post('/target', async(req,res)=>{
     const data = await Workout.findAll({
         where:{
@@ -66,6 +73,10 @@ router.post('/target', async(req,res)=>{
     
 })
 
+//POST:/user/workout/chartdata 는 유저의 운동볼륨 차트 정보를 보내는 라우터입니다. 
+//우선 parseDate, parseTarget 미들웨어를 거치며 변환된 날짜와 운동 부위를 바탕으로
+//데이터베이스에 접근합니다. 이후 기준 날짜와 가장 가까운 10일간의 데이터를 뽑아 사용자에게
+//보여주게 됩니다. Sequelize 의 Op연산자를 사용해 기준 날짜보다 작은 숫자의 날짜를 뽑아냈습니다.
 router.post('/chartdata', parseDate, parseTarget, async(req,res)=>{
     const data = await req.body.volumeTg.findAll({
         where:{
