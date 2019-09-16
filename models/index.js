a014ab8c9864873584162ab195618dcad647b10a
hanseongPark <hsung901231@gmail.com>
2019-09-16 22:53:32 +0900

    sequelize index.js Remark

--- models/index.js
+++ models/index.js
@@ -3,13 +3,20 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
+//시퀄라이즈를 사용하기 위한 index 설정 파일입니다. 우선 시퀄라이즈 객체에 인자로
+//config 파일의 설정을 불러옵니다. 이를 바탕으로 시퀄라이즈 변수를 설정합니다. config파일은
+//개발, 배포, 테스트로 설정이 나뉘어져있으며 process.env.NODE_ENV에 따라 개발과 배포 모드가
+//결정됩니다.
 const sequelize = new Sequelize(
   config.database, config.username, config.password, config,
 );
 
+//이후 db 객체에 시퀄라이즈 변수를 추가해줍니다.
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+//User, Calorie, Diet, Workout 데이터베이스와 각 부위별 볼륨 데이터베이스를
+//db 객체에 추가해줍니다.
 db.User = require('./user')(sequelize, Sequelize);
 db.Calorie = require('./calorie')(sequelize, Sequelize);
 db.Diet = require('./diet')(sequelize,Sequelize);
@@ -20,6 +27,9 @@ db.Shoulder = require('./shoulder')(sequelize,Sequelize);
 db.Leg = require('./leg')(sequelize,Sequelize);
 db.Core = require('./core')(sequelize,Sequelize);
 
+//다음은 데이터베이스들간의 관계설정입니다. User 데이터베이스를 제외한 나머지
+//데이터베이스는 User의 id를 외래키로 갖습니다. hasMany와 belongTo를 사용하여
+//데이터베이스들 간의 관계를 설정해줍니다.
 db.User.hasMany(db.Calorie, {foreignKey: 'user_id', sourceKey: 'id'});
 db.Calorie.belongsTo(db.User, {foreignKey: 'user_id', targetKey: 'id'});
 db.Diet.belongsTo(db.User,{foreignKey: 'user_id', targetKey: 'id'});
